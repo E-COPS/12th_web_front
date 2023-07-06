51cd8340663aa03ee7bb4a6b30b5c6a0751e8289
ahzero7d1 <ayg0418@naver.com>
2023-07-06 17:28:58 +0900

    home 페이지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import home_logo from "..//assets/home_logo.svg";
+import ecops_logo from "..//assets/ecops_logo.png";
 
 const Home = () => {
   return (
@@ -6,7 +7,9 @@ const Home = () => {
       <div className="home_logo">
         <img alt="home_logo" className="home_logo_img" src={home_logo}></img>
       </div>
-      <div className="home_description">안녕하세요</div>
+      <div className="home_description">
+        <img src={ecops_logo} alt="ecops_log" className="home_ecops_logo"></img>
+      </div>
     </div>
   );
 };
