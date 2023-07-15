5c18f0b5f4ab06d264f1e6568ed4ef1b8fb7a8d9
yslim21 <yslim317@gmail.com>
2023-07-16 02:41:50 +0900

    recruiting 수정2

--- src/pages/Recruiting.js
+++ src/pages/Recruiting.js
@@ -1,10 +1,10 @@
 import {useState} from "react";
-import recruiting_logo from "..//assets/recruiting_logo.svg";
-import recruiting_star from "..//assets/recruiting_star.svg";
-import recruiting_process from "..//assets/recruiting_process.svg";
-import recruiting_up from "..//assets/recruituing_up.svg";
-
-
+import recruiting_logo from "..//assets/Recruiting_img/recruiting_logo.svg";
+import recruiting_star from "..//assets/Recruiting_img/recruiting_star.svg";
+import recruiting_process from "..//assets/Recruiting_img/recruiting_process.svg";
+import recruiting_up from "..//assets/Recruiting_img/recruituing_up.svg";
+import recruiting_check from "../assets/Recruiting_img/recruiting_check.svg";
+import recruiting_ecops_img from "../assets/Recruiting_img/recruiting_ecops_img.svg";
 
 const Recruiting = () => {
 
@@ -26,15 +26,35 @@ const onClick = (index) => {
     return updateCheck;
   });
 
+ /* return (
+    <img className= "recruituing_up" src={recruiting_up} />
+  );*/
+
 };
 
 
 const CheckToggle= () => {
   return (
     <div className="answerToggle">
+      안녕
     </div>
   )
 }
+
+const CheckToggle1= () => {
+  return (
+    <div className="answerToggle">
+    </div>
+  )
+}
+
+const CheckToggle2= () => {
+  return (
+    <div className="answerToggle">
+    </div>
+  )
+}
+
 
 
   return (
@@ -73,7 +93,7 @@ const CheckToggle= () => {
               E-COPS is recruiting 12.5th recruitment.<br />
               Any student of Ewha Womans University who is interested in security and passionate can apply!!<br />
             </text>
-            <button className="applyButton button">12.5기 지원하기</button>
+            <button className="applyButton">12.5기 지원하기</button>
           </div>
         <div className="basicContainer">
           <div className="recruiting_process">
@@ -89,28 +109,36 @@ const CheckToggle= () => {
           <div className="basicContainer">
             <text className="black_bold_text2">자주 묻는 질문</text>
             <div className="toggleBox">
-              
               <button 
               className={IsColor[0]? "questionToggle2":"questionToggle"}
               onClick = {() => {onClick(0); handleClick(0);}}>
-                
-                {IsCheck[0]? <CheckToggle/>:null}
-                 
+              <text className="black_text5">Q: ECOPS는 어떤 동아리인가요?</text>  
               </button>
+              {IsCheck[0]?<div><CheckToggle /></div>:null}
+
               <button 
-              className={IsColor[1]? "questionToggle2":"questionToggle" }
-              onClick = {() => {onClick(1); handleClick(1); }}>
-                
-                {IsCheck[1]? <CheckToggle/>
-                :null}
+              className={IsColor[1]? "questionToggle2":"questionToggle"}
+              onClick = {() => {onClick(1); handleClick(1);}}>
+              <text className="black_text5">Q: ECOPS는 어떤 동아리인가요?</text>  
               </button>
+              {IsCheck[1]? <div><CheckToggle1/></div>:null}
+
               <button 
-              className={IsColor[2]? "questionToggle2":"questionToggle" }
+              className={IsColor[2]? "questionToggle2":"questionToggle"}
               onClick = {() => {onClick(2); handleClick(2);}}>
-                 
-                {IsCheck[2]? <CheckToggle/>:null}
+              <text className="black_text5">Q: ECOPS는 어떤 동아리인가요?</text>  
               </button>
+              {IsCheck[2]? <CheckToggle2/>:null}
             </div>
+          </div>
+        </div>
+        <div className="recruiting_extra">
+          <div className="basicContainer2">
+            <text className="grayText">더 궁금한 내용이나 <br/> 
+            문의사항 있으신가요?</text>
+            <img className="recruiting_check" src={recruiting_check}/>
+            <button className="applyButton" style={{marginRight:'73vw'}}>문의하기</button>
+            <img className="recruiting_ecops_img" src={recruiting_ecops_img} />
           </div>
         </div>
     </div>
