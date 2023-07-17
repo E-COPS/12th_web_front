a00097b98db64eca97229fbd13d5dccf6860426f
ahzero7d1 <ayg0418@naver.com>
2023-07-18 00:01:49 +0900

    MemberList grid 수정
    MemberList의 grid를 조금 수정해보았습니다

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -4,10 +4,9 @@ import MemberList from "./MemberList";
 import profile_example from "../assets/Member_img/profile_example.svg";
 
 const Member = () => {
-
   //const {id}=useParams();
   // const [year,setYear]=useState(11);
-  const dataId=useRef(0);
+  const dataId = useRef(0);
   // const [data, setData]=useState([]);
 
   // const getData=async()=>{
@@ -25,12 +24,12 @@ const Member = () => {
   //   });
   //   setData(initData);
   // }
- 
+
   // useEffect(()=>{
   //   getData();
   // },[])
 
-  const data =[
+  const data = [
     {
       year: "11",
       img: profile_example,
@@ -38,10 +37,10 @@ const Member = () => {
       email: "eileen0228@naver.com",
       comment: "안녕하세요12",
       insta_link: "https://www.instagram.com/hav_a_gr8_dae/",
-      git_link:"",
-      linkedin_link:"",
-      id:dataId.current++,
-      tf:true,
+      git_link: "",
+      linkedin_link: "",
+      id: dataId.current++,
+      tf: true,
     },
     {
       year: "11",
@@ -50,11 +49,10 @@ const Member = () => {
       email: "eileen0228@naver.com",
       comment: "안녕하세요11",
       insta_link: "https://www.instagram.com/hav_a_gr8_dae/",
-      git_link:"",
-      linkedin_link:"",
-      id:dataId.current++,
-      tf:true,
-
+      git_link: "",
+      linkedin_link: "",
+      id: dataId.current++,
+      tf: true,
     },
     {
       year: "11",
@@ -63,49 +61,54 @@ const Member = () => {
       email: "eileen0228@naver.com",
       comment: "안녕하세요13",
       insta_link: "",
-      git_link:"",
-      linkedin_link:"",
-      id:dataId.current++,
-      tf:false,
-    }
-  ]
-
-  const [listData,setListData]=useState([]);
-
-  const [isSelect, setIsSelect]=useState("");
-  const handleClick=(value)=>{
-    setListData(data.filter((it)=>it.year===value));
-    setIsSelect(value);
-  }
-
-  const btn=[
-    {year:'11'},
-    {year:'12'},
-    {year:'13'}
+      git_link: "",
+      linkedin_link: "",
+      id: dataId.current++,
+      tf: false,
+    },
   ];
-  
+
+  const [listData, setListData] = useState([]);
+
+  const [isSelect, setIsSelect] = useState("");
+  const handleClick = (value) => {
+    setListData(data.filter((it) => it.year === value));
+    setIsSelect(value);
+  };
+
+  const btn = [{ year: "13" }, { year: "12" }, { year: "11" }];
 
   return (
     <div className="Member">
       <h1 className="text">E-COPS의 멤버들을 소개합니다</h1>
-        <div className="buttonContainer">
-        {btn.map((item,idx)=>{
-          return(
-              <button key={idx} onClick={()=>{handleClick(item.year)}} className={"button"+`${isSelect===item.year?"Select":""}`} >{item.year}기</button>
+      <div className="buttonContainer">
+        {btn.map((item, idx) => {
+          return (
+            <button
+              key={idx}
+              onClick={() => {
+                handleClick(item.year);
+              }}
+              className={"button" + `${isSelect === item.year ? "Select" : ""}`}
+            >
+              {item.year}기
+            </button>
           );
         })}
-        </div>  
+      </div>
       <div className="MemberList">
-        {listData.map((it)=>(
-          <MemberList {...it}/>
-        ))}
+        <div className="Member_grid">
+          {listData.map((it) => (
+            <MemberList {...it} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-Member.defaultProps={
-  tf:Boolean,
-}
+Member.defaultProps = {
+  tf: Boolean,
+};
 
 export default Member;
