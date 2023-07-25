4d25ff73556a2453b4799607824e57fd67b26a05
ahzero7d1 <ayg0418@naver.com>
2023-07-25 15:32:15 +0900

    Activity 페이지 수정

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,26 +1,40 @@
 import git_logo from "..//assets/git_logo.png";
 import insta_logo from "..//assets/insta_logo.png";
 
+function get_Logo(link) {
+  return link ? (
+    <div className="sns_link">
+      <a href={link}>
+        <img className="sns_logo_button" alt="sns_logo" src={insta_logo} />
+      </a>
+    </div>
+  ) : (
+    <div></div>
+  );
+}
+
 const ActivityList = ({
   year,
   img,
-  project_name,
-  project_description,
-  insta_link,
-  git_link,
+  title,
+  author,
+  activity_description,
+  link,
 }) => {
   return (
-    <div className="activity_box">
+    <div
+      className="activity_box"
+      onClick={() => {
+        window.location.href = `${link}`;
+      }}
+    >
       <img className="member_img" alt="" src={img} />
       <div className="activity_info">
-        <text>{project_name}</text>
-        <a href={git_link}>
-          <img className="sns_logo_button" alt="sns_logo" src={git_logo} />
-        </a>
-        <a href={insta_link}>
-          <img className="sns_logo_button" alt="sns_logo" src={insta_logo} />
-        </a>
-        <text>{project_description}</text>
+        <text className="year_text">{year}기</text>
+        <text className="activity_title">{title}</text>
+        <text className="author">{author}</text>
+        <text className="activity_description">{activity_description}</text>
+        {get_Logo(`${link}`)}
       </div>
     </div>
   );
--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ActivityList from "../components/ActivityList";
 import activity_example_img from "../assets/activity_example_img.svg";
 
@@ -8,19 +8,89 @@ const Activity = () => {
   const activity_data = [
     {
       year: "12",
+      type: "보안뉴스",
       img: activity_example_img,
-      project_name: "프로젝트 2",
-      project_description: "12기 프로젝트 2입니다.",
-      insta_link: "",
+      title: "보안뉴스 1",
+      author: "글쓴이",
+      activity_description: "12기 프로젝트 2입니다.",
+      link: "https://www.instagram.com/ecops_ewha/",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "12",
+      type: "보안뉴스",
+      img: activity_example_img,
+      title: "보안뉴스 2",
+      author: "",
+      activity_description: "12기 프로젝트 3입니다.",
+      link: "",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "12",
+      type: "CTF",
+      img: activity_example_img,
+      title: "CTF 1",
+      author: "",
+      activity_description: "12기 CTF 1입니다.",
+      link: "",
       git_link: "",
       id: activityId.current++,
     },
     {
       year: "11",
+      type: "보안뉴스",
       img: activity_example_img,
-      project_name: "프로젝트 1",
-      project_description: "12기 프로젝트 1입니다.",
-      insta_link: "",
+      title: "보안뉴스 1",
+      author: "",
+      activity_description: "11기 보안뉴스 1입니다.",
+      link: "https://www.instagram.com/ecops_ewha/",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "13",
+      type: "보안뉴스",
+      img: activity_example_img,
+      title: "보안뉴스 1",
+      author: "글쓴이",
+      activity_description: "13기 보안뉴스 1입니다.",
+      link: "https://www.instagram.com/ecops_ewha/",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "13",
+      type: "보안뉴스",
+      img: activity_example_img,
+      title: "보안뉴스 2",
+      author: "",
+      activity_description: "13기 보안뉴스 2입니다.",
+      link: "",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "13",
+      type: "CTF",
+      img: activity_example_img,
+      title: "CTF 1",
+      author: "",
+      activity_description: "13기 CTF 1입니다.",
+      link: "",
+      git_link: "",
+      id: activityId.current++,
+    },
+    {
+      year: "13",
+      type: "CTF",
+      img: activity_example_img,
+      title: "CTF 2",
+      author: "",
+      activity_description: "13기 CTF 2입니다.",
+      link: "",
       git_link: "",
       id: activityId.current++,
     },
@@ -30,32 +100,64 @@ const Activity = () => {
 
   const [classData, setClassData] = useState("12");
 
+  //초기 화면 13기부터 나오도록 설정
+  useEffect(() => {
+    handleClick("13");
+  }, []);
+
   const handleClick = (value) => {
     setActivityData(activity_data.filter((it) => it.year === value));
     setClassData(value);
   };
 
+  const typeHandleClick = (value) => {
+    setActivityData(activity_data.filter((it) => it.type === value));
+    setClassData(value);
+  };
+
   const btn = [{ year: "13" }, { year: "12" }, { year: "11" }];
+  const type_btn = [{ type: "보안뉴스" }, { type: "CTF" }];
 
   return (
     <div className="Activity">
       <h1 className="title">Activity</h1>
       <div className="buttonContainer">
-        {btn.map((item, idx) => {
-          return (
-            <button
-              key={idx}
-              onClick={() => {
-                handleClick(item.year);
-              }}
-              className={
-                "button" + `${classData === item.year ? "Select" : ""}`
-              }
-            >
-              {item.year}기
-            </button>
-          );
-        })}
+        <div className="buttonRowContainer">
+          <div className="yearButton">
+            {btn.map((item, idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    handleClick(item.year);
+                  }}
+                  className={
+                    "button" + `${classData === item.year ? "Select" : ""}`
+                  }
+                >
+                  {item.year}기
+                </button>
+              );
+            })}
+          </div>
+          <div className="typeButton">
+            {type_btn.map((item, idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    typeHandleClick(item.type);
+                  }}
+                  className={
+                    "button" + `${classData === item.type ? "Select" : ""}`
+                  }
+                >
+                  {item.type}
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </div>
 
       <div className="ActivityList">
